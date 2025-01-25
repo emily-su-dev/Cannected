fbe7c7b60a281457bc4939d2d0bb1336706528ab
GraceSmerch <22fb33@queensu.ca>
2025-01-25 17:49:47 -0500

    test login frontend

--- client/src/login.js
+++ client/src/login.js
@@ -21,7 +21,99 @@ function Login() {
       )}
     </div>
   );
-}
+}  
+
+  const NameForm = () => {
+    const [name, setName] = useState('');
+  
+    const handleChange = (e) => {
+      setName(e.target.value);
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      alert(`Name submitted: ${name}`);
+    };
+  }  
+
+  const EmailForm = () => {
+    const [email, setEmail] = useState('');
+  
+    const handleChange = (e) => {
+      setEmail(e.target.value);
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      alert(`Email submitted: ${email}`);
+    };  
+  }
+
+  const PwdForm = () => {
+    const [password, setPwd] = useState('');
+  
+    const handleChange = (e) => {
+      setPwd(e.target.value);
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      alert(`Password submitted: ${password}`);
+    };    
+  }
+
+  function LoginPage() {
+
+    return (
+      <div>
+        <body>
+          <h1>LOGIN</h1>
+          <p>Enter you information below to </p>
+            <p>login to your account.</p>
+
+            <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Enter your name:</label>
+              <input 
+                type="text" 
+                id="name" 
+                name="name" 
+                value={name} 
+                onChange={handleChange} 
+              />
+            <button type="submit">Submit</button>
+            </form>
+
+            <form onSubmit={handleSubmit}>
+            <label htmlFor="email">Enter your email:</label>
+              <input 
+                type="text" 
+                id="email" 
+                name="email" 
+                value={email} 
+                onChange={handleChange} 
+              />
+            <button type="submit">Submit</button>
+            </form>
+
+            <form onSubmit={handleSubmit}>
+            <label htmlFor="password">Enter your password:</label>
+              <input 
+                type="text" 
+                id="password" 
+                name="password" 
+                value={password} 
+                onChange={handleChange} 
+              />
+            <button type="submit">Submit</button>
+            </form>
+            
+            <a href = "!Midpage.html"> 
+            <button>log in</button>
+          </a>
+            
+        </body>
+      </div>
+    );
+  }
 
 export default Login;
-
