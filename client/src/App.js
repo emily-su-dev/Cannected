53b97f136648dde428ceafbe4b1869efdfcb1e1d
GraceSmerch <22fb33@queensu.ca>
2025-01-25 17:14:08 -0500

    test homepage front

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,31 @@ function App() {
 
       {/* Step 4: Use the Login component */}
       <Login />
+
+    <body>
+      <header>
+        <t>CANnected</t>
+      </header>
+      <header>
+        <a href="!Login.html">
+          <button style="background-color: navy; color: white; border: none; padding: 10px 20px; border-radius: 10px; font-size: 15px; text-align: center; display: inline;">
+            Login
+          </button>
+        </a>
+    </header>
+
+      <section>
+        <h2>About Us</h2>
+        <p>CANnected connects can donators with can collectors </p> 
+        <p>to ease the return and recycling of alcoholic cans in </p>
+        <p> Kingston.</p>
+      </section>
+      <footer>
+        <p>&copy; 2025 My First Website</p>
+      </footer>
+    </body>
     </div>
-  );
+    );
 }
 
 export default App;
