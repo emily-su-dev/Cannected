398da9c42245df9dc224cb689bca7b83190d8d17
GraceSmerch <22fb33@queensu.ca>
2025-01-25 22:57:48 -0500

    make collection page

---
+++ client/src/Collection.js
@@ -0,0 +1,45 @@
+
+/*class Collection {
+    constructor() {
+
+        const click = () => {
+            alert('CLICK!');
+        }
+
+        constructor(props); {
+            super(props);
+            this.state = { minutes: "5", cans: "5" };
+        }
+
+        return (
+
+            <div>
+                <p style="color: green; font-size: 20px">YOU ARE CURRENTLY COLLECTING</p>
+
+                <p>This pickup is {this.state.minutes} minutes away with {this.state.cans} cans</p>
+                <button type="button" color="red" onClick={click}>ACCEPT</button>
+                <button type="button" color="red" onClick={click}>DENY</button>
+            </div>
+        );
+    }
+}*/
+
+function Collection(){
+    
+    return (
+
+        <div>
+            <p style="color: green; font-size: 20px">YOU ARE CURRENTLY COLLECTING</p>
+
+            <p>This pickup is {props.minutes} minutes away with {props.cans} cans</p>
+            <button type="button" color="red" onClick={click}>ACCEPT</button>
+            <button type="button" color="red" onClick={click}>DENY</button>
+        </div>
+    );
+}
+
+const click = () => {
+    alert('CLICK!');
+}
+
+export default Collection;
