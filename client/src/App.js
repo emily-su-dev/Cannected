99c403011c04d84684875375255c4c64c26a9654
Emily <emilysucanada@gmail.com>
2025-01-25 23:31:49 -0500

    add home page

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import { Routes, Route } from 'react-router-dom';  // Correctly import BrowserRo
 import Login from './login';  // Import Login component
 import Profile from './profile';  // Import Profile component
 import SignUp from './signup';
+import Home from './home';
+import Collection from './Collection';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />  {/* Default route */}
+      <Route path="/" element={<Home />} />  {/* Default route */}
       <Route path="/login" element={<Login />} />  {/* Login route */}
       <Route path="/profile" element={<Profile />} />  {/* Profile route */}
     </Routes>
